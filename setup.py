2134eff5f9d9c1fba84d0964330e34d18a9ed290
Jyumpp <hdhdog@live.com>
2020-01-14 13:40:41 -0500

    update setup.py release information

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dynamixel_controller",
-    version=0.3,
+    version='0.3.post1',
     packages=find_packages(),
     author="Hunter Halloran (Jyumpp)",
     author_email="hdh20267@uga.edu",
