634e9b2ed55f379e1367b9c9749fb5db0a5a0d73
Jyumpp <hdhdog@live.com>
2020-01-14 11:43:32 -0500

    Changes to ready for upload to PyPi

---
+++ NOTICE.md
@@ -0,0 +1,4 @@
+This product includes software developed at
+Robotis Inc. (http://www.robotis.us/) 
+under the Apache version 2.0 license
+from https://github.com/ROBOTIS-GIT/DynamixelSDK
--- setup.py
+++ setup.py
@@ -9,9 +9,21 @@ setup(
     package_data={
         '': ['*.json'],
     },
+    install_requires=[
+        'json',
+        'pkg_resources',
+    ],
+    download_url='https://github.com/Jyumpp/dynamixel-controller/archive/v_01.tar.gz',
     url="https://github.com/Jyumpp/dynamixel-controller",
     keywords="dynamixel, dxl, dynamixel_sdk, motor controller",
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
         'License :: Apache License'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
